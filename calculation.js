fd4903139f8b9cbf67780d4a15df50c845e77840
Ashraful Islam <ashrafulislam21211@gmail.com>
2022-08-22 15:27:24 +0600

    Error Validation Done 5 select player

--- calculation.js
+++ calculation.js
@@ -3,67 +3,78 @@ var messi = {
     imgUrl:"image/messi.jpg",
     numberOfGoal: 124,
     numberOfAssist: 255,
+    id: "1"
     
 }
 var rolandho = {
     name: "cristiano ronaldo",
     imgUrl:"image/ronaldho.jpg",
     numberOfGoal: 104,
-    numberOfAssist:155,
+    numberOfAssist: 155,
+    id: "2"
 }
 var coutinho = {
     name: "coutinho",
     imgUrl:"image/coutinho.jpg",
     numberOfGoal: 84,
-    numberOfAssist:55,
+    numberOfAssist: 55,
+    id: "3"
 }
 var dipol = {
     name: "depul",
     imgUrl:"image/dipol.jpg",
     numberOfGoal: 4,
-    numberOfAssist:5,
+    numberOfAssist: 5,
+    id: "4"
 }
 var kane = {
     name: "kane",
     imgUrl:"image/kane.jpg",
     numberOfGoal: 134,
-    numberOfAssist:135,
+    numberOfAssist: 135,
+    id: "5"
 }
 var kante = {
     name: "kante",
     imgUrl:"image/kante.jpg",
     numberOfGoal: 134,
-    numberOfAssist:355,
+    numberOfAssist: 355,
+    id: "6"
 }
 var lewandowski = {
     name: "lewandowski",
     imgUrl:"image/lewandowski.jpg",
     numberOfGoal: 204,
-    numberOfAssist:255,
+    numberOfAssist: 255,
+    id: "7"
 }
 var maria = {
     name: "maria",
     imgUrl:"image/maria.jpg",
     numberOfGoal: 204,
-    numberOfAssist:55,
+    numberOfAssist: 55,
+    id: "8"
 }
 var neymar = {
     name: "neymar",
     imgUrl:"image/neymar.jpg",
     numberOfGoal: 124,
-    numberOfAssist:255,
+    numberOfAssist: 255,
+    id: "9"
 }
 var ozil = {
     name: "cristiano ozil",
     imgUrl:"image/ozil.jpg",
     numberOfGoal: 104,
-    numberOfAssist:155,
+    numberOfAssist: 155,
+    id: "10"
 }
 var ramos = {
     name: "ramos",
     imgUrl:"image/ramos.jpg",
     numberOfGoal: 104,
-    numberOfAssist:155,
+    numberOfAssist: 155,
+    id:"11"
 }
 
 
@@ -82,7 +93,7 @@ function dynamicPlayer(player) {
                     <p class="mr-3">Goal:Goal:${player.numberOfGoal}</p>
                     <p>Assist:${player.numberOfAssist}</p>
                 </div>
-                <button type="submit" class= "text-white bg-sky-600 w-48 mt-4 h-8" id="Nameplayers" onclick = 'playerSelect(${stringyfy})'>Select</button>
+                <button type="submit" class= "text-white bg-sky-600 w-48 mt-4 h-8" id=${player.id} onclick = 'playerSelect(${stringyfy})'>Select</button>
             </div>
     
     `
--- calculation2.js
+++ calculation2.js
@@ -1,5 +1,5 @@
 let serial = 0;
-function playerSelect(playerNameSelect,event) {
+function playerSelect(playerNameSelect) {
     let playerName = playerNameSelect.name;
     console.log(playerName);
     let Table = document.getElementById('table')
@@ -7,11 +7,22 @@ function playerSelect(playerNameSelect,event) {
     let td = document.createElement('td');
     let td2 = document.createElement('td');
      serial = serial+1;
-    td.innerText = serial;
-    td.classList.add('text-center');
-    Table.append(tr);
-    tr.appendChild(td);
-    td2.innerText = playerName;
-    td2.classList.add('text-center');
-    tr.appendChild(td2);
+    if (serial <= 5) {
+        td.innerText = serial;
+        td.classList.add('text-center');
+         Table.append(tr);
+        tr.appendChild(td);
+        td2.innerText = playerName;
+        td2.classList.add('text-center');
+        tr.appendChild(td2);  
+        var x = document.getElementById(playerNameSelect.id);
+        console.log(x);
+        x.setAttribute('disabled', true);
+        x.style.backgroundColor = "gray";
+    }
+    else {
+        alert("You cannot add More than 5 players");
+        
+    }
+    
 }
