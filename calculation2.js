e564277131734cbc4e5fe670ab22c5f4f2ad30aa
Ashraful Islam <ashrafulislam21211@gmail.com>
2022-08-22 16:31:35 +0600

    player Calculation done

--- calculation2.js
+++ calculation2.js
@@ -6,8 +6,9 @@ function playerSelect(playerNameSelect) {
     let tr = document.createElement('tr');
     let td = document.createElement('td');
     let td2 = document.createElement('td');
-     serial = serial+1;
-    if (serial <= 5) {
+    
+    if (serial < 5) {
+        serial = serial+1;
         td.innerText = serial;
         td.classList.add('text-center');
          Table.append(tr);
@@ -26,3 +27,24 @@ function playerSelect(playerNameSelect) {
     }
     
 }
+document.getElementById('playerPriceButton').addEventListener('click', function () {
+     let calculationinputbox = document.getElementById('calcInput');
+    let perPlayerPrice = calculationinputbox.value;
+    let totalPlayer = serial;
+    let totalPrice = perPlayerPrice * totalPlayer;
+    let totalPlayerPrice = document.getElementById('totalPlayerPrice');
+    totalPlayerPrice.innerText = totalPrice;
+})
+document.getElementById('finalCalculation').addEventListener('click', function () {
+    let managerPrice = document.getElementById('manager');
+    let coachPrice = document.getElementById('coach');
+    let totalPlayerPrice = document.getElementById('totalPlayerPrice');
+    let final = document.getElementById('finalPrice');
+    let totaPlayerFinalPrice = parseInt(totalPlayerPrice.innerText);
+    let managerTotalPrice = parseInt(managerPrice.value);
+    let coachTotalPrice =parseInt( coachPrice.value);
+    let totalPrice = parseInt(managerTotalPrice + coachTotalPrice + totaPlayerFinalPrice);
+    console.log(totalPrice)
+    final.innerText = totalPrice;
+})
+
